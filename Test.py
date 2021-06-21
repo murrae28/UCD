5ef3362fbba33b5a030ab8442375071032b71640
murrae28 <emma.murray28@gmail.com>
2021-06-21 16:42:29 +0100

    data cleansing

--- Test.py
+++ Test.py
@@ -1,1 +1,9 @@
-print("hello")
+import pandas as pd
+
+data = pd.read_csv("Netflixs/netflix_titles.csv")
+
+new_data = data.drop_duplicates(subset=["director", "cast"])
+
+print(data.shape, new_data.shape)
+
+
