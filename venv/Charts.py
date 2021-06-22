38f2e96d520ed7b13b294917c8f2135c735f7a11
murrae28 <emma.murray28@gmail.com>
2021-06-22 20:31:38 +0100

    Charts test 2

--- venv/Charts.py
+++ venv/Charts.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 fig,ax=plt.subplots()
 x=[1,2,3,4,5]
+#x2=[2,3,6,7,9]
 y=[2,4,6,8,10]
-ax.plot(x,y)
+#y2=[1,2,3,4,7]
+ax.plot(x,y, color = "lime", linestyle="--", marker="*")
+#ax.bar(x,y2)
+#ax.scatter(x2,y2)
 plt.show()
