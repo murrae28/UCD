9d9e2a91edb04ee3d156f94bbe1ffe5497e012be
murrae28 <emma.murray28@gmail.com>
2021-06-23 19:43:13 +0100

    seaborn test

--- Chart Test.py
+++ Chart Test.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
 data = pd.read_csv("Netflixs/netflix_titles.csv")
+datachart = (data[["release_year", "duration"]])
+#data =sns.load_dataset("Netflixs/netflix_titles.csv")
+fig, ax = plt.subplots()
+sns.heatmap(datachart)
+#sns.lineplot(x=data["director"].head(5), y=data["rating"].head(5), marker='o')
 
-x= data["country"].head(5)
-y= data["rating"].head(5)
-
-fig,ax=plt.subplots()
-ax.plot(x,y)
+#plt.legend(loc='lower right')
+# ax.plot(x,y)
 plt.show()
-
