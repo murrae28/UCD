f07d5170e2a5a8809990a1f48d26b6539b7f5814
murrae28 <emma.murray28@gmail.com>
2021-07-11 17:09:49 +0100

    Identifying what rows has a A type rating

--- UCDPA_EmmaMurray/Main.py
+++ UCDPA_EmmaMurray/Main.py
@@ -1,16 +1,22 @@
 import pandas as pd
 #importing file from kaggle
 data=pd.read_csv("corporate_rating.csv")
+
 #running small checks to determine size of dataset
 print(data.head())
 print(data.shape)
+
 #checking for missing values
 missing_values_count = data.isnull().sum()
 print(missing_values_count[0:5])
+
 #If missing data is detecting, drop entire row
 droprows= data.dropna()
 print(data.shape,droprows.shape)
+
 #Drop all rows that are duplicate
 drop_duplicates= data.drop_duplicates()
 print(data.shape,drop_duplicates.shape)
 
+#Identifying what rows have a A type rating
+is_a_rating=print(data['Rating'].isin(['A', 'AA', 'AAA']))
