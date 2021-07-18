816a5db024213419cb25e0c3498533eec85703bf
murrae28 <emma.murray28@gmail.com>
2021-07-18 12:13:44 +0100

    Extracted the year from the date column

--- UCDPA_EmmaMurray/Main.py
+++ UCDPA_EmmaMurray/Main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 #importing file from kaggle
 data=pd.read_csv("corporate_rating.csv")
 
@@ -28,6 +30,17 @@ print(data.loc[(data.Name == 'Whirlpool Corporation') & (data.Rating == 'AAA')])
 
 #Counting ratings by company name
 print(data.groupby(['Name', 'Rating']).Rating.count())
+
+data =data.loc[data['Sector'] == 'Finance']
+Company= ['Equifax, Inc.', 'Marsh & McLennan Companies, Inc.', 'Aon plc', 'S&P Global Inc.', 'Progressive Corporation (The)', 'Loews Corporation']
+data.loc[data['Name'].isin(Company)]
+print(data.head())
+print(data.shape)
+#extratcing the year from the date column
+data['Year'] = pd.DatetimeIndex(data['Date']).year
+print(data.Year)
+
+
 
 
 #avg_ratio_rating =pd.pivot_table(data, index =['Name'], columns='Rating', values = 'currentRatio', aggfunc='mean')
